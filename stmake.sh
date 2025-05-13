d3fcbb4f3b0e59cdad800e766c6f5930912d79c4
strayb1ts <pragmaticprogram@proton.me>
2025-05-12 20:41:26 -0600

    updated stmake to be more useful and less prone to error

--- stmake.sh
+++ stmake.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-read -p "Enter the filename (should be a .bin file): " filename
-sudo st-flash --reset write $filename 0x8000000
+numargs=$#
+if [ ${numargs} -gt 1 ]; then
+	echo "too many arguments! call with no arguments to be asked what file to use or call with the bin file you want to use"
+	exit 1 <&2
+elif [ ${numargs} -eq 1 ]; then
+	sudo st-flash --reset write $1 0x8000000
+	exit 0 >&2
+else
+	read -p "Enter the filename (should be a .bin file): " filename
+	sudo st-flash --reset write $filename 0x8000000
+	exit 0 >&2
+fi
