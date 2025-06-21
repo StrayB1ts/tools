0895dbd89251bbd7c598ec1fe5660a572a163cbd
pragmaticprog <pragmaticprogram@proton.me>
2025-06-20 20:19:04 -0600

    added charging indicator to bat.sh

--- bat.sh
+++ bat.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 chars=", "
-acpi -b | awk 'NR==1{print$4}' | cut -c 1-4 | sed "s/[$chars]//g"
+chargestate=$(acpi -b | awk 'NR==1{print$3}' | cut -c 1-3)
+charge=$(acpi -b | awk 'NR==1{print$4}' | cut -c 1-4 | sed "s/[$chars]//g")
+if [ $chargestate = 'Cha' ]; then
+	echo "$charge Charging"
+else
+	echo "$charge"
+fi	
