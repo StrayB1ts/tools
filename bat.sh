74781b0302cda1519ec0f3b015188f85ce93e4e3
pragmaticprog <pragmaticprogram@proton.me>
2025-06-22 22:38:41 -0600

    modified bat.sh to also output battery fully charged if charging and above 80%

--- bat.sh
+++ bat.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-chars=", "
+chars=", %"
 chargestate=$(acpi -b | awk 'NR==1{print$3}' | cut -c 1-3)
 charge=$(acpi -b | awk 'NR==1{print$4}' | cut -c 1-4 | sed "s/[$chars]//g")
-if [ $chargestate = 'Cha' ]; then
-	echo "$charge Charging"
+if [ "$chargestate" = 'Cha' ] && [ "$charge" -gt 80 ] ; then
+	echo "Battery fully charged"
+elif [ "$chargestate" = "Cha" ]; then
+	echo $charge% Charging
 else
-	echo "$charge"
+	echo $charge%
 fi	
