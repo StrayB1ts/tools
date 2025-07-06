1aa08981ae60fcf9585d4ff4bb1f8817feec27e4
pragmaticprog <pragmaticprogram@proton.me>
2025-07-06 01:26:32 -0600

    actual fix for the problem from last commit, also a clarification: it adds a new battery when plugged in using the barrel jack not type c

--- bat.sh
+++ bat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chars=", %:"
-if [ $(acpi -b | awk 'NR==1{print$3}') = 'Not' ];then
+if [ $(acpi -b | awk 'NR==1{print$3}') = 'Not' ] ; then
 	chargestate=$(acpi -b | awk 'NR==2{print$3}' | cut -c 1-3)
 	charge=$(acpi -b | awk 'NR==1{print$5}' | cut -c 1-4 | sed "s/[$chars]//g")
 	bat=$(acpi -b | awk 'NR==2{print$2}' | cut -c 1-4 | sed "s/[$chars]//g")
@@ -10,8 +10,8 @@ else
 	bat=$(acpi -b | awk 'NR==1{print$2}' | cut -c 1-4 | sed "s/[$chars]//g")
 
 fi	
-if [ "$bat" = '0' ]; then
-	if [ "$chargestate" = 'Cha'] && [ "$charge" -gt 80 ] ; then
+if [ "$bat" = '0' ] ; then
+	if [ "$chargestate" = 'Cha' ] && [ "$charge" -gt 80 ] ; then
 		echo "Battery fully charged"
 	elif [ "$chargestate" = "Cha" ]; then
 		echo $charge% Charging
@@ -19,7 +19,7 @@ if [ "$bat" = '0' ]; then
 		echo $charge%
 	fi
 else
-	if [ "$chargestate" = 'Ful'] && [ "$charge" -gt 80 ] ; then
+	if [ "$chargestate" = 'Ful' ] && [ "$charge" -gt 80 ] ; then
 		echo "Battery fully charged"
 	elif [ "$chargestate" = "Cha" ]; then
 		echo $charge% Charging
