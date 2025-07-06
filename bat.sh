1e70a88cb367cad6730a6e0799c72fba2d4be423
pragmaticprog <pragmaticprogram@proton.me>
2025-07-06 01:18:10 -0600

    modified bat.sh to handle up to 2 batteries as for some reason charging my laptop adds a battery and it broke the script sometimes

--- bat.sh
+++ bat.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
-chars=", %"
-chargestate=$(acpi -b | awk 'NR==1{print$3}' | cut -c 1-3)
-charge=$(acpi -b | awk 'NR==1{print$4}' | cut -c 1-4 | sed "s/[$chars]//g")
-if [ "$chargestate" = 'Cha' ] && [ "$charge" -gt 80 ] ; then
-	echo "Battery fully charged"
-elif [ "$chargestate" = "Cha" ]; then
-	echo $charge% Charging
+chars=", %:"
+if [ $(acpi -b | awk 'NR==1{print$3}') = 'Not' ];then
+	chargestate=$(acpi -b | awk 'NR==2{print$3}' | cut -c 1-3)
+	charge=$(acpi -b | awk 'NR==1{print$5}' | cut -c 1-4 | sed "s/[$chars]//g")
+	bat=$(acpi -b | awk 'NR==2{print$2}' | cut -c 1-4 | sed "s/[$chars]//g")
 else
-	echo $charge%
+	chargestate=$(acpi -b | awk 'NR==1{print$3}' | cut -c 1-3)
+	charge=$(acpi -b | awk 'NR==1{print$4}' | cut -c 1-4 | sed "s/[$chars]//g")
+	bat=$(acpi -b | awk 'NR==1{print$2}' | cut -c 1-4 | sed "s/[$chars]//g")
+
 fi	
+if [ "$bat" = '0' ]; then
+	if [ "$chargestate" = 'Cha'] && [ "$charge" -gt 80 ] ; then
+		echo "Battery fully charged"
+	elif [ "$chargestate" = "Cha" ]; then
+		echo $charge% Charging
+	else
+		echo $charge%
+	fi
+else
+	if [ "$chargestate" = 'Ful'] && [ "$charge" -gt 80 ] ; then
+		echo "Battery fully charged"
+	elif [ "$chargestate" = "Cha" ]; then
+		echo $charge% Charging
+	else
+		echo $charge%
+	fi	
+fi
